13f786aa461ca1176320867d76c66b987bb769e4
jcable <julian.cable@bbc.co.uk>
2023-09-04 21:59:24 +0100

    fix: use esbuild

--- src/lib/members_boats.js
+++ src/lib/members_boats.js
@@ -1,8 +1,6 @@
-
-export default function membersBoats(boats=[], members=[]) {
-    return boats.filter((b) => b.owners?.length > 0).map((b) => {
-      const owners = b.owners.map((o) => members.find((m) => m.id === o && o)).filter((o) => o);
-      return { ...b, owners, id: b.oga_no };
-    }).filter((b) => b.owners.length > 0);
-  }
-  
+export default function membersBoats(boats = [], members = []) {
+  return boats.filter((b) => b.owners?.length > 0).map((b) => {
+    const owners = b.owners.map((o) => members.find((m) => m.id === o && o)).filter((o) => o);
+    return { ...b, owners, id: b.oga_no };
+  }).filter((b) => b.owners.length > 0);
+}
