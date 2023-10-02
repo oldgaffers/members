aeaa00345a785ff2f1def2277d0e4bafab6db0bb
jcable <julian.cable@bbc.co.uk>
2023-10-02 17:46:20 +0100

    fix: lint & wip

--- src/components/SetSkipperProfile.jsx
+++ src/components/SetSkipperProfile.jsx
@@ -1,49 +1,55 @@
 import React, { useState } from 'react';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
-import { Stack } from '@mui/system';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormLabel, Typography } from '@mui/material';
-import { ReactReallyTinyEditor as ReactTinyEditor } from '@ogauk/react-tiny-editor'
+import {
+  Button, Dialog, DialogActions, DialogContent,
+  DialogContentText, DialogTitle, FormLabel, Stack, Typography,
+} from '@mui/material';
+import { ReactReallyTinyEditor as ReactTinyEditor } from '@ogauk/react-tiny-editor';
 
-export default function SetSkipperProfile({ user, onCancel, onSubmit, open }) {
-    const [text, setText] = useState('');
+export default function SetSkipperProfile({
+  onCancel, onSubmit, open,
+}) {
+  const [text, setText] = useState('');
 
-    const handleSubmit = () => {
-        console.log('N', text);
-        // onSubmit(user, node.innerHtml)
-    };
+  const handleSubmit = () => {
+    onSubmit(text);
+  };
 
-    return (
-        <Dialog
-            open={open}
-            aria-labelledby="dialog-configure-crewing"
-            maxWidth='md'
-            fullWidth
-        >
-            <ScopedCssBaseline>
-                <DialogTitle>Configure Crewing Opportunities</DialogTitle>
-                <DialogContent>
-                        <Typography>
-                        Welcome to our newest feature.
-                        </Typography>
-                        <Typography>
-                        This feature is intended for skippers who regularly or occasionally need crew.
-                        </Typography>
-                    <Stack>
-                        <FormLabel><Typography>Create or Edit your Skipper's profile here.</Typography></FormLabel>
-                        <ReactTinyEditor html='A <strong>test</strong> message'
-                        onChange={setText}
-                        />
-                    </Stack>
-                    <DialogContentText>
-                        Your skipper's profile will be visible on the boat register to members who view
-                        the records of boats you have announced.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onCancel}>Cancel</Button>
-                    <Button onClick={handleSubmit}>Submit</Button>
-                </DialogActions>
-            </ScopedCssBaseline>
-        </Dialog>
-    );
+  return (
+    <Dialog
+      open={open}
+      aria-labelledby="dialog-configure-crewing"
+      maxWidth="md"
+      fullWidth
+    >
+      <ScopedCssBaseline>
+        <DialogTitle>Configure Crewing Opportunities</DialogTitle>
+        <DialogContent>
+          <Typography>
+            Welcome to our newest feature.
+          </Typography>
+          <Typography>
+            This feature is intended for skippers who regularly or occasionally need crew.
+          </Typography>
+          <Stack>
+            <FormLabel>
+              <Typography>Create or Edit your Skipper&rsquo;s profile here.</Typography>
+            </FormLabel>
+            <ReactTinyEditor
+              html="A <strong>test</strong> message"
+              onChange={setText}
+            />
+          </Stack>
+          <DialogContentText>
+            Your skipper&rsquo;s profile will be visible on the boat register to members who view
+            the records of boats you have announced.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
+      </ScopedCssBaseline>
+    </Dialog>
+  );
 }
--- src/components/UpdateMyDetails.jsx
+++ src/components/UpdateMyDetails.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
-import { Stack } from '@mui/system';
+import Stack from '@mui/material/Stack';
 import EditIcon from '@mui/icons-material/Edit';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useQuery, gql } from '@apollo/client';
-import { Button, CircularProgress, List, ListItem, Snackbar, Typography } from '@mui/material';
+import {
+  Button, CircularProgress, List, ListItem, Snackbar, Typography,
+} from '@mui/material';
 import RoleRestricted from './RoleRestricted';
 import { getFilterable, postGeneralEnquiry } from '../lib/api';
 import { SuggestLogin } from './LoginButton';
@@ -19,157 +22,188 @@ const MEMBER_QUERY = gql(`query members($members: [Int]!) {
     members(members: $members) {
         salutation firstname lastname member id GDPR 
         smallboats status telephone mobile area town
-        interests email primary
+        interests email primary profile
     }
   }`);
 
 // TODO ReJoin
 
-function email_indication(record) {
-    if ((record?.email || '').includes('@')) {
-        return `The OGA will email you at ${record.email}.`
-    }
-    return 'You haven\'t provided an email address so you won\t get any emails.';
+function emailIndication(record) {
+  if ((record?.email || '').includes('@')) {
+    return `The OGA will email you at ${record.email}.`;
+  }
+  return 'You haven\'t provided an email address so you won\t get any emails.';
 }
 
-function Update({ openInterestDialog, openContactDialog, openSkipperProfileDialog}) {
-    return <Stack direction='row' spacing={2} sx={{ margin: 2 }}>
-        <Button size="small"
-            endIcon={<EditIcon />}
-            variant="contained"
-            color="primary" onClick={() => openInterestDialog(true)}>
-            Update My Interests
-        </Button>
-        <Button size="small"
-            endIcon={<EditIcon />}
-            variant="contained"
-            color="primary" onClick={() => openContactDialog(true)}>
-            Contact the Membership Secretary
-        </Button>
-        <Button size="small"
-            endIcon={<EditIcon />}
-            variant="contained"
-            color="primary" onClick={() => openSkipperProfileDialog(true)}>
-            Set My Skipper Profile
-        </Button>
-    </Stack>;
+function Update({ openInterestDialog, openContactDialog, openSkipperProfileDialog }) {
+  return (
+    <Stack direction="row" spacing={2} sx={{ margin: 2 }}>
+      <Button
+        size="small"
+        endIcon={<EditIcon />}
+        variant="contained"
+        color="primary"
+        onClick={() => openInterestDialog(true)}
+      >
+        Update My Interests
+      </Button>
+      <Button
+        size="small"
+        endIcon={<EditIcon />}
+        variant="contained"
+        color="primary"
+        onClick={() => openContactDialog(true)}
+      >
+        Contact the Membership Secretary
+      </Button>
+      <Button
+        size="small"
+        endIcon={<EditIcon />}
+        variant="contained"
+        color="primary"
+        onClick={() => openSkipperProfileDialog(true)}
+      >
+        Set My Skipper Profile
+      </Button>
+    </Stack>
+  );
 }
 
 function MyDetails() {
-    const [openInterests, setOpenInterests] = useState(false);
-    const [openContact, setOpenContact] = useState(false);
-    const [openSkipperProfile, setOpenSkipperProfile] = useState(false);
-    const [snackBarOpen, setSnackBarOpen] = useState(false);
-    const [boats, setBoats] = useState();
-    const { user } = useAuth0();
-    const id = user?.["https://oga.org.uk/id"];
-    const memberNo = user?.["https://oga.org.uk/member"];
-    const memberResult = useQuery(MEMBER_QUERY, { variables: { members: [memberNo] } });
+  const [openInterests, setOpenInterests] = useState(false);
+  const [openContact, setOpenContact] = useState(false);
+  const [openSkipperProfile, setOpenSkipperProfile] = useState(false);
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [boats, setBoats] = useState();
+  const { user } = useAuth0();
+  const id = user?.['https://oga.org.uk/id'];
+  const memberNo = user?.['https://oga.org.uk/member'];
+  const memberResult = useQuery(MEMBER_QUERY, { variables: { members: [memberNo] } });
 
-    const handleSubmitInterests = (newData) => {
-        setOpenInterests(false);
-        postGeneralEnquiry('member', 'profile', { ...newData })
-            .then((response) => {
-                setSnackBarOpen(true);
-            })
-            .catch((error) => {
-                // console.log("post", error);
-                // TODO snackbar from response.data
-            });
+  const handleSubmitInterests = (newData) => {
+    setOpenInterests(false);
+    postGeneralEnquiry('member', 'profile', { ...newData })
+      .then((response) => {
+        console.log(response);
+        setSnackBarOpen(true);
+      })
+      .catch((error) => {
+        console.log('post', error);
+        // TODO snackbar from response.data
+      });
+  };
+
+  const hco = () => alert('Crew');
+
+  const hho = () => alert('hire');
+
+  useEffect(() => {
+    if (!boats) {
+      getFilterable()
+        .then((r) => {
+          setBoats(r);
+        }).catch((e) => console.log(e));
     }
+  }, [boats]);
 
-    const handleSubmitContact = (text) => {
-        setOpenContact(false);
-        postGeneralEnquiry('member', 'profile', { user, text })
-            .then((response) => {
-                setSnackBarOpen(true);
-            })
-            .catch((error) => {
-                // console.log("post", error);
-                // TODO snackbar from response.data
-            });
-    }
+  if (memberResult.loading) {
+    return <CircularProgress />;
+  }
 
-    const handleSubmitSkipperProfile = (text) => {
-        setOpenContact(false);
-        postGeneralEnquiry('member', 'skipper_profile', { user, text })
-            .then((response) => {
-                setSnackBarOpen(true);
-            })
-            .catch((error) => {
-                // console.log("post", error);
-                // TODO snackbar from response.data
-            });
-    }
+  if (!memberResult.data) {
+    return <CircularProgress />;
+  }
+  const members = memberResult.data.members.filter((m) => m.member === memberNo);
+  const myRecord = members.find((m) => m.id === id);
+  const myBoats = membersBoats(boats, members);
+  const primary = members.find((m) => m.primary);
 
-    const hco = () => alert('Crew')
+  const handleSubmitContact = (text) => {
+    setOpenContact(false);
+    postGeneralEnquiry('member', 'profile', { user, text })
+      .then((response) => {
+        setSnackBarOpen(true);
+      })
+      .catch((error) => {
+        // console.log("post", error);
+        // TODO snackbar from response.data
+      });
+  };
 
-    const hho = () => alert('hire')
+  const handleSubmitSkipperProfile = (text) => {
+    setOpenContact(false);
+    postGeneralEnquiry('member', 'skipper_profile', { user, text })
+      .then((response) => {
+        setSnackBarOpen(true);
+      })
+      .catch((error) => {
+        // console.log("post", error);
+        // TODO snackbar from response.data
+      });
+  };
 
-    useEffect(() => {
-        if (!boats) {
-            getFilterable()
-                .then((r) => {
-                    setBoats(r);
-                }).catch((e) => console.log(e));
-        }
-    }, [boats]);
-
-    if (!memberResult.data) {
-        return <CircularProgress />
-    }
-    const members = memberResult.data.members.filter((m) => m.member === memberNo);
-    const myRecord = members.find((m) => m.id === id);
-    const myBoats = membersBoats(boats, members);
-    const primary = members.find((m) => m.primary);
-
-    return (
-        <>
-            <Typography variant='h6'>Hi {user.name}.</Typography>
-            <List>
-                <ListItem>your membership number is {memberNo}</ListItem>
-                <ListItem>
-                    {email_indication(myRecord)}
-                </ListItem>
-                <ListItem>
-                    {(myRecord.primary) ?
-                        `You are the 'primary' member in this membership.
+  console.log(myRecord);
+  return (
+    <>
+      <Typography variant="h6">
+        Hi
+        {' '}
+        {myRecord.firstname}
+        .
+      </Typography>
+      <List>
+        <ListItem>
+          your membership number is
+          {' '}
+          {memberNo}
+        </ListItem>
+        <ListItem>
+          {emailIndication(myRecord)}
+        </ListItem>
+        <ListItem>
+          {(myRecord.primary)
+            ? `You are the 'primary' member in this membership.
                         Gaffers Log will be sent to you.`
-                        :
-                        `You are not the 'primary' member in this membership.
-                        Gaffers Log will be sent to ${primary.firstname} ${primary.lastname}`
-                    }
+            : `You are not the 'primary' member in this membership.
+                        Gaffers Log will be sent to ${primary.firstname} ${primary.lastname}`}
 
-                </ListItem>
-            </List>
-            <Stack direction='column'>
-                <MemberStatus key={memberNo} memberNo={memberNo} members={members} />
-                <MembersByMembership members={members} boats={myBoats}/>
-                <BoatsByMembership boats={myBoats} onSetHireOptions={hho} onsetCrewingOptions={hco} showContactButton={false} />
-                <Update
-                openInterestDialog={setOpenInterests}
-                openContactDialog={setOpenContact}
-                openSkipperProfileDialog={setOpenSkipperProfile}
-                />
-            </Stack>
-            <UpdateInterestsDialog user={myRecord} onSubmit={handleSubmitInterests} onCancel={() => setOpenInterests(false)} open={openInterests} />
-            <ContactTheMembershipSecretary user={user} onSubmit={handleSubmitContact} onCancel={() => setOpenContact(false)} open={openContact} />
-            <SetSkipperProfile user={user} onSubmit={handleSubmitSkipperProfile} onCancel={() => setOpenSkipperProfile(false)} open={openSkipperProfile} />
-            <Snackbar
-                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                open={snackBarOpen}
-                autoHideDuration={2000}
-                onClose={() => setSnackBarOpen(false)}
-                message="Thanks, we'll get back to you."
-                severity="success"
-            />
-        </>
-    );
+        </ListItem>
+      </List>
+      <Stack direction="column">
+        <MemberStatus key={memberNo} memberNo={memberNo} members={members} />
+        <MembersByMembership members={members} boats={myBoats} />
+        <BoatsByMembership
+          boats={myBoats}
+          onSetHireOptions={hho}
+          onsetCrewingOptions={hco}
+          showContactButton={false}
+        />
+        <Update
+          openInterestDialog={setOpenInterests}
+          openContactDialog={setOpenContact}
+          openSkipperProfileDialog={setOpenSkipperProfile}
+        />
+      </Stack>
+      <UpdateInterestsDialog user={myRecord} onSubmit={handleSubmitInterests} onCancel={() => setOpenInterests(false)} open={openInterests} />
+      <ContactTheMembershipSecretary user={user} onSubmit={handleSubmitContact} onCancel={() => setOpenContact(false)} open={openContact} />
+      <SetSkipperProfile user={user} onSubmit={handleSubmitSkipperProfile} onCancel={() => setOpenSkipperProfile(false)} open={openSkipperProfile} />
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        open={snackBarOpen}
+        autoHideDuration={2000}
+        onClose={() => setSnackBarOpen(false)}
+        message="Thanks, we'll get back to you."
+        severity="success"
+      />
+    </>
+  );
 }
 
 export default function UpdateMyDetails() {
-    return <>
-        <SuggestLogin />
-        <RoleRestricted role='member'><MyDetails /></RoleRestricted>
-    </>;
+  return (
+    <>
+      <SuggestLogin />
+      <RoleRestricted role="member"><MyDetails /></RoleRestricted>
+    </>
+  );
 }
