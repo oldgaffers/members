f195965bc0ee21abf718112af599f59dbcdbf9fe
jcable <julian.cable@bbc.co.uk>
2023-10-28 16:01:05 +0100

    fix: boats

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
-import { FormControlLabel, FormGroup, Switch, Typography } from '@mui/material';
+import {
+  FormControlLabel, FormGroup, Switch, Typography,
+} from '@mui/material';
 import { gql, useQuery } from '@apollo/client';
 import { useAuth0 } from '@auth0/auth0-react';
 import { SuggestLogin } from './LoginButton';
@@ -9,21 +11,34 @@ import RoleRestricted from './RoleRestricted';
 import memberPredicate from '../lib/membership';
 import MembersAndBoats from './MembersAndBoats';
 import membersBoats from '../lib/members_boats.mjs';
+import { getFilterable } from '../lib/api';
 
 function MembersList() {
   const [excludeNotPaid, setExcludeNotPaid] = useState(false);
   const [excludeNoConsent, setExcludeNoConsent] = useState(true);
+  const [boats, setBoats] = useState(undefined);
   const { user } = useAuth0();
 
   const roles = user['https://oga.org.uk/roles'];
   const memberNo = user['https://oga.org.uk/member'];
-
   const membersResult = useQuery(gql`query members { members { 
     salutation firstname lastname member id GDPR 
     status telephone mobile town area interests smallboats
    } }`);
 
-  const boats = [];
+  useEffect(() => {
+    if (!boats) {
+      getFilterable()
+        .then((r) => {
+          setBoats(r);
+        }).catch((e) => console.log(e));
+    }
+  }, [boats]);
+
+  if (!boats) {
+    return <CircularProgress />;
+  }
+
   if (membersResult.loading) {
     return <CircularProgress />;
   }
@@ -33,25 +48,34 @@ function MembersList() {
   }
 
   const { members } = membersResult.data;
-  const ybmembers = members.filter((m) => memberPredicate(m.id, m, excludeNotPaid, excludeNoConsent));
+  const ybmembers = members
+    .filter((m) => memberPredicate(m.id, m, excludeNotPaid, excludeNoConsent));
 
   const wboats = membersBoats(boats, ybmembers);
 
   const handleNotPaidSwitchChange = (event, newValue) => {
     setExcludeNotPaid(newValue);
-  }
+  };
 
   const handleNoConsentSwitchChange = (event, newValue) => {
     setExcludeNoConsent(newValue);
-  }
+  };
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Typography variant='h6'>Hi {user.name} ({memberNo}).</Typography>
+        <Typography variant="h6">
+          Hi
+          {' '}
+          {user.name}
+          {' '}
+          (
+          {memberNo}
+          ).
+        </Typography>
         <FormGroup>
-          {roles.includes['editor'] ? <FormControlLabel control={<Switch onChange={handleNotPaidSwitchChange} checked={excludeNotPaid} />} label="Exclude not paid" /> : ''}
-          {roles.includes['editor'] ? <FormControlLabel control={<Switch onChange={handleNoConsentSwitchChange} checked={excludeNoConsent} />} label="Exclude no Consent" /> : ''}
+          {roles.includes.editor ? <FormControlLabel control={<Switch onChange={handleNotPaidSwitchChange} checked={excludeNotPaid} />} label="Exclude not paid" /> : ''}
+          {roles.includes.editor ? <FormControlLabel control={<Switch onChange={handleNoConsentSwitchChange} checked={excludeNoConsent} />} label="Exclude no Consent" /> : ''}
         </FormGroup>
       </Box>
       <MembersAndBoats members={ybmembers} boats={wboats} />
@@ -60,9 +84,10 @@ function MembersList() {
 }
 
 export default function Members() {
-  return <>
-    <SuggestLogin />
-    <RoleRestricted role='member'><MembersList /></RoleRestricted>
-  </>
-    ;
+  return (
+    <>
+      <SuggestLogin />
+      <RoleRestricted role="member"><MembersList /></RoleRestricted>
+    </>
+  );
 }
