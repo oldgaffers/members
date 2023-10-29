682899c235b76d1b303b45b8c7ad3c126f652572
jcable <julian.cable@bbc.co.uk>
2023-10-29 08:35:00 +0000

    fix: add scopes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import OGAProvider from "./components/OGAProvider";
+import OGAProvider from './components/OGAProvider';
 import Members from './components/Members';
 import Boats from './components/Boats';
 import UpdateMyDetails from './components/UpdateMyDetails';
@@ -11,23 +11,23 @@ import UpdateMyDetails from './components/UpdateMyDetails';
 function Wrapper({ children }) {
   return (
     <React.StrictMode>
-    <Auth0Provider
-      domain="dev-uf87e942.eu.auth0.com"
-      clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
-      authorizationParams={{
-        redirect_uri: window.location.origin + window.location.pathname,
-        audience: "https://oga.org.uk/boatregister",
-        scope: 'member',
-      }}
-      useRefreshTokens={true}
-      useRefreshTokensFallback={true}
-      cacheLocation='localstorage'
-    >
-      <OGAProvider>
-        {children}
-      </OGAProvider>
-    </Auth0Provider>
-  </React.StrictMode >
+      <Auth0Provider
+        domain="dev-uf87e942.eu.auth0.com"
+        clientId="Mlm45jI7zvoQXbLSYSNV8F1qI1iTEnce"
+        authorizationParams={{
+          redirect_uri: window.location.origin + window.location.pathname,
+          audience: 'https://oga.org.uk/boatregister',
+          scope: 'openid profile email member',
+        }}
+        useRefreshTokens
+        useRefreshTokensFallback
+        cacheLocation="localstorage"
+      >
+        <OGAProvider>
+          {children}
+        </OGAProvider>
+      </Auth0Provider>
+    </React.StrictMode>
   );
 }
 
@@ -36,7 +36,7 @@ const members = document.getElementById('members');
 const boats = document.getElementById('members_boats');
 
 if (details) {
-  ReactDOM.createRoot(details).render(<Wrapper><UpdateMyDetails/></Wrapper>);
+  ReactDOM.createRoot(details).render(<Wrapper><UpdateMyDetails /></Wrapper>);
 }
 
 if (members) {
