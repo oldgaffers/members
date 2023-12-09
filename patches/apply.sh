77b39078c3204eb2df2520d4815f797b85a9a4e7
jcable <julian.cable@bbc.co.uk>
2023-12-09 16:27:41 +0000

    fix: wip

--- patches/apply.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo $PWD
-cp ./patches/react-calendar/package.json node_modules/@toast-ui/calendar/package.json
-cp ./patches/calendar/package.json node_modules/@toast-ui/calendar/package.json
