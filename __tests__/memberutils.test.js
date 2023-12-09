430b72e52fa1e37735a1c905ff89a8018a1a37c7
jcable <julian.cable@bbc.co.uk>
2023-12-09 15:37:06 +0000

    fix: testing

--- __tests__/memberutils.test.js
+++ __tests__/memberutils.test.js
@@ -1,7 +1,9 @@
+import { expect, test } from 'vitest'
+import { membershipType } from '../src/lib/utils';
+
 test('type', async () => {
-  const utils = (await import('../src/lib/utils'));
-  expect(utils.membershipType({ type: 'Honorary' })).toBe('You are an Elder Gaffer or other honorary member');
-  expect(utils.membershipType({ type: 'Single', payment: 'Direct Debit' })).toBe('Your membership type is Single and you pay by Direct Debit.');
-  expect(utils.membershipType({ type: 'Junior', yob: 1963, payment: 'Direct Debit' })).toBe('Your membership type is Junior and you pay by Direct Debit but we have your year of birth recorded as 1963 so you should change to Single or Family membership.');
-  expect(utils.membershipType({ type: 'Junior', yob: 2010, payment: 'Direct Debit' })).toBe('Your membership type is Junior and you pay by Direct Debit.');
+  expect(membershipType({ type: 'Honorary' })).toBe('You are an Elder Gaffer or other honorary member');
+  expect(membershipType({ type: 'Single', payment: 'Direct Debit' })).toBe('Your membership type is Single and you pay by Direct Debit.');
+  expect(membershipType({ type: 'Junior', yob: 1963, payment: 'Direct Debit' })).toBe('Your membership type is Junior and you pay by Direct Debit but we have your year of birth recorded as 1963 so you should change to Single or Family membership.');
+  expect(membershipType({ type: 'Junior', yob: 2010, payment: 'Direct Debit' })).toBe('Your membership type is Junior and you pay by Direct Debit.');
 });
