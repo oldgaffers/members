d7c955b94adfc6d170b0ee3c23a39db6f4e20b4a
jcable <julian.cable@bbc.co.uk>
2023-12-09 16:04:36 +0000

    fix: patch toast

---
+++ patches/apply.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo $PWD
+cp ./patches/react-calendar/package.json node_modules/@toast-ui/calendar/package.json
+cp ./patches/calendar/package.json node_modules/@toast-ui/calendar/package.json
