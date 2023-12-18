a2f5911fd74a5e54d8712531f786859aa4953acf
jcable <julian.cable@bbc.co.uk>
2023-12-18 09:00:27 +0000

    fix: claim boats

--- src/BoatsByMembership.tsx
+++ src/BoatsByMembership.tsx
@@ -5,30 +5,79 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button, Switch, Typography } from '@mui/material';
-import { Boat, boatUrl } from './lib/api.mts';
+import { Autocomplete, Button, CircularProgress, Link, Stack, Switch, TextField, Typography } from '@mui/material';
+import { Boat, boatUrl, getFilterable } from './lib/api.mts';
 import { ownerList, ownerValueGetter } from './lib/ownership.mts';
+import { useEffect, useState } from 'react';
 
 type BoatsByMembershipProps = {
   boats: Boat[]
   onChange: Function
 }
 
+function BoatsByMembershipHeader({ boats }: { boats: Boat[] }) {
+  if (boats.length === 0) {
+    return <>
+      <Typography>You don't have any boats registered to your membership.</Typography>
+      </>;
+  }
+  return <Typography variant="h6">
+    The following
+    {' '}
+    {(boats.length > 1) ? 'boats are ' : 'boat is '}
+    registered against your membership:
+  </Typography>
+}
+
+function ChooseABoat({ boats }: { boats: Boat[] }) {
+  const [filterable, setFilterable] = useState<Boat[] | undefined>();
+
+  useEffect(() => {
+    if (!filterable) {
+      getFilterable()
+        .then((r) => {
+          setFilterable(r);
+        }).catch((e) => console.log(e));
+    }
+  }, [filterable]);
+
+  if (!filterable) {
+    return <CircularProgress />;
+  }
+
+  const ex = boats.map((b) => b.oga_no);
+
+  const names = filterable.map((b) => `${b.name} (${b.oga_no})`).filter((b) => !ex.includes(b.oga_no));
+  return <>
+    <Autocomplete
+      options={names}
+      renderInput={(params) => <TextField name="type" {...params} label="Boat" />}
+    />
+    <TextField label='Year you acquired her'></TextField>
+    <Button sx={{ width: 150  }} >Claim this boat</Button>
+  </>;
+}
+
+function BoatsByMembershipFooter({ boats }: { boats: Boat[] }) {
+
+    return <Stack sx={{ marginTop: 1 }} spacing={1}>
+      <Typography variant='h6'>If you have {(boats.length === 0)? 'a boat ' : 'other boats '}
+      you can get them added.
+      </Typography>
+      <Typography variant='h6'>
+      If your boat is already on the register you can claim ownership here.</Typography>
+      <ChooseABoat boats={boats} />
+      <Typography variant='h6'>If your boat isn't on the list, then you add it on the <Link component='span' href='/browse_the_register/index.html'>boat register</Link>.</Typography>
+  
+      </Stack>;
+}
+
 export default function BoatsByMembership({
   boats, onChange,
 }: BoatsByMembershipProps) {
-  if (boats.length === 0) {
-    return <Typography>You don't have any boats registered to your membership</Typography>;
-  }
-
   return (
     <>
-      <Typography variant="h6">
-        Your
-        {' '}
-        {(boats.length > 1) ? 'boats are' : 'boat is'}
-        :
-      </Typography>
+      <BoatsByMembershipHeader boats={boats} />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -50,12 +99,12 @@ export default function BoatsByMembership({
                 </TableCell>
                 <TableCell align="right">{row.oga_no}</TableCell>
                 <TableCell align="right">{ownerList(row.owners)}</TableCell>
-                <TableCell align="right">{ownerValueGetter({ value: row.owners})}</TableCell>
+                <TableCell align="right">{ownerValueGetter({ value: row.owners })}</TableCell>
                 <TableCell align="right"><Switch onChange={(e) => onChange(row, 'hire', e.target.checked)} checked={row.hire} /></TableCell>
                 <TableCell align="right"><Switch onChange={(e) => onChange(row, 'crewing', e.target.checked)} checked={row.crewing} /></TableCell>
                 <TableCell align="right">
                   <Button
-                    sx= {{padding: "5px"}}
+                    sx={{ padding: "5px" }}
                     size="small"
                     component="a"
                     href={boatUrl(row.oga_no, {
@@ -73,11 +122,12 @@ export default function BoatsByMembership({
           </TableBody>
         </Table>
       </TableContainer>
-      <Typography>* changes you make here have immediate effect, 
+      <Typography>* changes you make here have immediate effect,
         they don't need to be approved by the boat register editors.
         If you want to set more text about the crewing opportunities you are offering or the conditions of hire,
         do it using the edit form on the boat's detail page on the boat register.
-        </Typography>
+      </Typography>
+      <BoatsByMembershipFooter boats={boats} />
     </>
   );
 }
