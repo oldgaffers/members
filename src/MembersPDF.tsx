520dbc00c8103dda9c47cd70365e4f6064416519
jcable <julian.cable@bbc.co.uk>
2024-10-08 06:37:27 +0100

    fix: linting

--- src/MembersPDF.tsx
+++ src/MembersPDF.tsx
@@ -2,7 +2,6 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import { Member } from './lib/membership.mts';
 import { Boat } from './lib/api.mts';
 import { phoneGetter } from './lib/utils.mts';
-import { Key } from 'react';
 
 const styles = StyleSheet.create({
   page: {
