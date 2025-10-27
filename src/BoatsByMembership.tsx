8b1a6416e53b3bc2bc3d0997f6102405ce1f00b3
Julian Cable <julian.cable@yahoo.com>
2025-10-27 09:31:00 +0000

    Update BoatsByMembership.tsx

--- src/BoatsByMembership.tsx
+++ src/BoatsByMembership.tsx
@@ -10,6 +10,7 @@ import { Boat, boatUrl, getFilterable, postGeneralEnquiry } from './lib/boatregi
 import { ownerList, ownerValueGetter } from './lib/ownership.mts';
 import { useEffect, useState } from 'react';
 import { Member } from './lib/membership.mts';
+import ChooseABoat from './ChooseABoat';
 
 type BoatsByMembershipProps = {
   boats: Boat[]
@@ -29,41 +30,6 @@ function BoatsByMembershipHeader({ boats }: { boats: Boat[] }) {
     {(boats.length > 1) ? 'boats are ' : 'boat is '}
     registered against your membership:
   </Typography>
-}
-
-function ChooseABoat({ boats, onClick }: { boats: Boat[], onClick: Function }) {
-  const [filterable, setFilterable] = useState<Boat[] | undefined>();
-  const [year, setYear] = useState<string>();
-  const [inputValue, setInputValue] = useState<string>();
-
-  useEffect(() => {
-    if (!filterable) {
-      getFilterable()
-        .then((r) => {
-          setFilterable(r);
-        }).catch((e) => console.log(e));
-    }
-  }, [filterable]);
-
-  if (!filterable) {
-    return <CircularProgress />;
-  }
-
-  const ex = boats.map((b) => b.oga_no);
-
-  const names = filterable.filter((b) => !ex.includes(b.oga_no)).map((b) => `${b.name} (${b.oga_no})`);
-
-  return <>
-    <Autocomplete
-      options={names}
-      inputValue={inputValue ?? ''}
-      onInputChange={(_event, newInputValue: string) => {
-        setInputValue(newInputValue);
-      }}      renderInput={(params) => <TextField  name="type" {...params} label="Boat" />}
-    />
-    <TextField onChange={(e) => setYear(e.target.value)} label='Year you acquired her'></TextField>
-    <Button sx={{ width: 150  }} onClick={() => onClick(inputValue, year)}>Claim this boat</Button>
-  </>;
 }
 
 function BoatsByMembershipFooter({ boats, member }: { boats: Boat[], member: Member }) {
