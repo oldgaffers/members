6db273d964072f90354cd4a62592c18b54a96784
Julian Cable <julian.cable@yahoo.com>
2025-10-27 07:53:58 +0000

    Update UpdateMyDetails.tsx

--- src/UpdateMyDetails.tsx
+++ src/UpdateMyDetails.tsx
@@ -95,7 +95,7 @@ async function fetchBoatData(members: Member[], token: string) {
         const owned = membersBoats(r, members);
 
         const details = await Promise.all(owned.map((b) => getBoat(b.oga_no, token)));
-        const got: Boat[] = details.filter((b) => b);
+        const got: Boat[] = details.filter((b is Boat) => b);
         const p = got.map((b: Boat) => {
           const n = owned.find((l) => l.oga_no === b.oga_no);
           // set options explicity so switches are always controlled
