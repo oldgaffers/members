fc2901fa2916ffc7a960c4e687ce830039a9aa49
jcable <julian.cable@yahoo.com>
2026-01-15 17:52:03 +0000

    fix: add private documents

---
+++ src/PrivateDocument.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, Key } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import RoleRestricted from "./RoleRestricted";
+
+const apiWeb = 'https://5li1jytxma.execute-api.eu-west-1.amazonaws.com/default/doc';
+
+export async function getApiWeb(doc: string, accessToken: string) {
+  return (await fetch(
+    `${apiWeb}?doc_name=${doc}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      }
+    }
+  )).text();
+}
+
+export default function Members({ name }: { name: string }) {
+  const [text, setText] = useState<string | undefined>();
+  const { getAccessTokenSilently } = useAuth0();
+  const [token, setToken] = useState<string | undefined>();
+
+  useEffect(() => {
+    async function getToken() {
+      if (!token) {
+        const tok = await getAccessTokenSilently();
+        setToken(tok);
+      }
+    }
+    getToken();
+  }, [token]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const p = await getApiWeb(name, token);
+      setText(p);
+    }
+    if (!text) {
+      getData();
+    }
+  }, [text]);
+
+  return (
+    <>
+      <RoleRestricted role="member">
+        <div dangerouslySetInnerHTML={{ __html: text }}></div>
+      </RoleRestricted>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import Boats from './Boats.tsx';
 import FindCrew from './FindCrew.tsx';
 import FindACruise from './FindACruise.tsx';
 import CustomMap from './CustomMap.tsx';
+import PrivateDocument from './PrivateDocument.tsx';
 // import { registerServiceWorker } from './rsw.mjs';
 
 declare global {
@@ -18,6 +19,7 @@ const boats = document.getElementById('members_boats');
 const crewfinder = document.getElementById('crewfinder');
 const cruisefinder = document.getElementById('cruisefinder');
 const map = document.getElementById('map');
+const doc = document.getElementById('doc');
 
 if (details) {
   ReactDOM.createRoot(details).render(<App><UpdateMyDetails /></App>);
@@ -44,5 +46,29 @@ if (map) {
   const markers = window.markers ?? [];
   ReactDOM.createRoot(map).render(<App><CustomMap height={800} markers={markers} scope={attr['scope'] ?? 'public'} /></App>);
 }
+
+if (doc) {
+  ReactDOM.createRoot(doc).render(<App><PrivateDocument  /></App>);
+}
+
+// convert paragraph elements with <<xxxx>> to divs so that we can nest p and divs inside them
+// we need this because most editors don't have html access.
+function handleParagraphs() {
+  const allparas = document.getElementsByTagName('p');
+  for (let i = 0; i < allparas.length; i++) {
+    const p = allparas.item(i);
+    if (!p) continue;
+    const text = p.innerText || '';
+    const q = text.match(/^<<(.*?):(.*)>>$/);
+    if (q?.length === 3) {
+      const [, component, arglist] = q;
+      const args = arglist.split(':');
+      const al = args.map((a, index) => `data-oga-arg${index}="${a.trim()}"`).join(' ');
+      p.outerHTML = `<div id="${component}" ${al}></div>`;
+    }
+  }
+}
+
+handleParagraphs();
 
 // registerServiceWorker();
